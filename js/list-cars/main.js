a037cf8ae0f07200dbeca2dda2d2a817ef957013
Douglas Soares <douglasmaismcpe@gmail.com>
2022-03-23 20:35:42 -0300

    Get List of Cars by API

---
+++ js/list-cars/main.js
@@ -0,0 +1,41 @@
+const baseURL = "http://localhost:5500";
+
+async function onLoad() {
+  const cars = await getRequest("car");
+
+  cars.forEach((car) => {
+    document.getElementById("list").innerHTML += `
+      <div class="car">
+        <div class="car-content">
+          <a href="#">
+            <div class="car-image">
+              <img src=${car.images === null ? "./assets/images/RS5Coupe.png" : car.images[0]} alt=${car.name}>
+            </div>
+
+            <hr>
+
+            <div class="info">
+              <span>${car.brand.name}</span>
+
+              <div class="info-content">
+                <div class="group-info">
+                  <h3>${car.name}</h3>
+                  <span>R$ ${car.price}</span>
+                  <img src="./assets/icons/energy.svg" alt="Elétrico">
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    `;
+  });
+}
+
+async function getRequest(url) {
+  const response = await axios.get(`${baseURL}/${url}`);
+
+  return response.data;
+}
+
+onLoad();
