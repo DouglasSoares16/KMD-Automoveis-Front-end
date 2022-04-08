7ca9c8046a33dacf948d5961d0b2437b2ed7c933
Douglas Soares <douglasmaismcpe@gmail.com>
2022-04-08 19:12:30 -0300

    Verified if User is Authenticated

---
+++ js/list-cars/isLogged.js
@@ -0,0 +1,53 @@
+
+
+async function isLogged() {
+  const token = window.localStorage.getItem("@kmd_auto:token");
+  const boxLogin = document.querySelector(".box-login");
+
+  if (!token) {
+    boxLogin.innerHTML = `
+      <p>
+        <a href="./login-user.html">
+          Faça Login
+        </a>
+      </p>
+
+      <div class="user">
+        <img src="./assets/icons/perfil.svg" alt="Photo">
+      </div>
+    `;
+  }
+
+  try {
+    const { data: user } = await axios.get(`http://localhost:5500/user/verify/authenticate/${token}`);
+
+    boxLogin.innerHTML = `
+      <p>
+        <a href="./perfil-user.html">
+          ${user.name}
+        </a>
+      </p>
+
+      <div class="user">
+        <img src=${user.avatar === undefined ? "./assets/icons/perfil.svg" : user.avatar} alt=${user.name}>
+      </div>
+    `;
+
+  } catch (error) {
+    console.error(error.message);
+
+    boxLogin.innerHTML = `
+      <p>
+        <a href="./login-user.html">
+          Faça Login
+        </a>
+      </p>
+
+      <div class="user">
+        <img src="./assets/icons/perfil.svg" alt="Photo">
+      </div>
+    `;
+  }
+}
+
+isLogged();
