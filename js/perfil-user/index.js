46e0654cf55f5dccf3129f6dc4985151fa914f9a
Douglas Soares <douglasmaismcpe@gmail.com>
2022-04-18 17:14:12 -0300

    Show the Bought History of User

--- js/perfil-user/index.js
+++ js/perfil-user/index.js
@@ -1,19 +1,19 @@
 
 
 async function onLoad() {
-	const token = window.localStorage.getItem("@kmd_auto:token");
+  const token = window.localStorage.getItem("@kmd_auto:token");
 
-	axios.defaults.headers.common.authorization = `Bearer ${token}`;
+  axios.defaults.headers.common.authorization = `Bearer ${token}`;
 
-	try {
-		const response = await axios.get("http://localhost:5500/user/profile");
+  try {
+    const response = await axios.get("http://localhost:5500/user/profile");
 
-		document.querySelector(".wrapper-image").innerHTML = `
+    document.querySelector(".wrapper-image").innerHTML = `
 			<img src="./assets/icons/perfil.svg" alt=${response.data.user.name}>
 			<img src="./assets/icons/changed-photo.svg" alt="Alterar" class="change-img">
 		`;
 
-		document.getElementById("update-user").innerHTML = `
+    document.getElementById("update-user").innerHTML = `
 			<div class="input-block">
 				<div class="icon-input">
 					<img src="./assets/icons/perfil.svg" alt="Perfil">
@@ -39,56 +39,83 @@ async function onLoad() {
 			</div>
 		`;
 
-		if (response.data.cars === null) {
-			document.querySelector(".wrapper-history").innerHTML = `
+    if (response.data.cars === null) {
+      document.querySelector(".wrapper-history").innerHTML = `
 				<br>
 				<p>Você não possui nenhuma compra, compre um carro na loja clicando <a href="./list-cars.html">Aqui</a></p>
 			`;
-		}
+    }
+    else {
+      response.data.cars.forEach((car) => {
+        document.querySelector(".wrapper-history").innerHTML += `
+          <div class="car">
+            <div class="car-content">
+              <a href="./show-car.html?carId=${car.id}">
+                <div class="car-image">
+                  <img src=${car.images === null ? "./assets/images/RS5Coupe.png" : car.images[0]} alt=${car.name}>
+                </div>
+                <div class="info">
+                  <span>${car.category.name}</span>
+                  <div class="info-content">
+                    <div class="group-info">
+                      <h3>${car.name}</h3>
+                      <div class="group-price">
+                        <span>R$ ${car.price}</span>
+                        <img src="./assets/icons/energy.svg" alt="Elétrico">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        `;
+      });
+    }
 
-	} catch (error) {
-		window.location.href = "login-user.html";
-	}
+  } catch (error) {
+    window.location.href = "login-user.html";
+  }
 }
 
 const Form = {
-	getValues() {
-		return {
-			name: document.querySelector("input#name").value,
-			email: document.querySelector("input#email").value,
-			cnh: document.querySelector("input#cnh").value,
-		}
-	},
+  getValues() {
+    return {
+      name: document.querySelector("input#name").value,
+      email: document.querySelector("input#email").value,
+      cnh: document.querySelector("input#cnh").value,
+    }
+  },
 
-	validateFields() {
-		const { email, cnh, name } = Form.getValues();
+  validateFields() {
+    const { email, cnh, name } = Form.getValues();
 
-		if (
-			name.trim() === "" ||
-			email.trim() === "" ||
-			cnh.trim() === ""
-		) {
-			throw new Error("Preencha todos os campos")
-		}
-	},
+    if (
+      name.trim() === "" ||
+      email.trim() === "" ||
+      cnh.trim() === ""
+    ) {
+      throw new Error("Preencha todos os campos")
+    }
+  },
 
-	async updateUser(event) {
-		event.preventDefault();
+  async updateUser(event) {
+    event.preventDefault();
 
-		Form.validateFields();
+    Form.validateFields();
 
-		try {
-			const data = Form.getValues();
+    try {
+      const data = Form.getValues();
 
-			await axios.put("http://localhost:5500/user", data);
+      await axios.put("http://localhost:5500/user", data);
 
-			window.location.href = "perfil-user.html";
-		} catch (error) {
-			const { data } = error.response;
+      window.location.href = "perfil-user.html";
+    } catch (error) {
+      const { data } = error.response;
 
       alert(data.message);
-		}
-	},
+    }
+  },
 };
 
 onLoad().then();
